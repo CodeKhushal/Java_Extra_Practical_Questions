6b27bf847bd827936fc06c23f6946977cf635ea6
Khushal_88044 <68191677+CodeKhushal@users.noreply.github.com>
2021-05-28 19:54:01 +0530

    Create checkPrime.java

---
+++ 6-CheckPrime/checkPrime.java
@@ -0,0 +1,27 @@
+//This program will repeatedly ask the user for an integer and tell that the entered number is prime or not.
+import java.util.Scanner;
+public class checkPrime {
+
+    static public void checkPrime()
+    {
+        int num;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number that you want to check prime or not: ");
+        num = scan.nextInt();
+        int count = 0;
+        for (int i = 2; i <= num - 1; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+        if (count == 0)
+            System.out.println("Number " + num + " is prime");
+        else
+            System.out.println("Number " + num + " is not prime");
+        checkPrime();
+    }
+
+    public static void main(String[] args) {
+            checkPrime();
+    }
+}
