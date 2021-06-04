797c1f6c29579c5ea159696b48acbbb7067afb0e
Khushal_88044 <68191677+CodeKhushal@users.noreply.github.com>
2021-06-04 17:16:59 +0530

    Update Main.java

--- Practical on 3rd June/Q-2/P/Main.java
+++ Practical on 3rd June/Q-2/P/Main.java
@@ -1,13 +1,12 @@
 package P;
 
-import P1.*;
-import P2.*;
+import P1.TwoDim;
+import P2.ThreeDim;
 public class Main {
     public static void main(String[] args) {
-        TwoDim ref;
-        ref = new TwoDim(8, 12);
-        System.out.println(ref);
-        ref = new ThreeDim(6, 9, 18);
-        System.out.println(ref);
+        TwoDim ref1 = new TwoDim(8, 12);
+        System.out.println(ref1.toString());
+        ThreeDim ref2 = new ThreeDim(6, 9, 18);
+        System.out.println(ref2.toString());
     }
 }
